bc02d17d19c14a6669860d43b12e67bfbcf4e496
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-08-05 03:43:52 +0000

    re-submit the task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.find(task_params)
+    @task = Task.new(task_params)
     if @task.save
       flash[:success] = 'YOU SUCCESS TO MAKE NEW TASK!'
       redirect_to root_path
